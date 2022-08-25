9019339e825db8f0250f12a38bf3559daa3831a7
Daniela Leguari <danielaleguari@outlook.com>
2022-08-25 16:43:19 -0300

    ajustado arquivo, html e criado arquivo js

---
+++ js/main.js
@@ -0,0 +1,20 @@
+function efetuaLogin(email, password) {
+  if (email && password) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+document.querySelector("#form").addEventListener("submit", function (e) {
+  const email = document.getElementById("email").value;
+  //console.log(email);
+  const password = document.getElementById("password").value;
+  const sucessoLogin = efetuaLogin(email, password);
+  //console.log(sucessoLogin);
+  if (!sucessoLogin) {
+    e.preventDefault(); // previne que o form seja submetido
+    return;
+  }
+  alert("Logou com sucesso!");
+});
